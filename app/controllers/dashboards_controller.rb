0950274fc496b186eceac5b2deabe077d4da6b86
Sahil Goonraz <sahil.goonraz@solirius.com>
2025-10-14 13:25:09 +0100

    Add dashboard view

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,22 @@
 class DashboardsController < ApplicationController
+  def index
+    @students = Student.all
+    @lessons = Lesson.includes(:student).all # Eager load students for rate/duration calculation
+
+    @total_students = @students.count
+
+    @lessons_this_week = @lessons.where('date >= ? AND date <= ?', Time.current.beginning_of_week, Time.current.end_of_week).count
+    
+    @monthly_revenue = @lessons.where(paid: true, date: Time.current.beginning_of_month..Time.current.end_of_month)
+                               .sum { |l| l.duration * (l.student&.rate || 0) }
+
+    @outstanding_payments_sum = @lessons.where(paid: false)
+                                        .sum { |l| l.duration * (l.student&.rate || 0) }
+    
+    @upcoming_lessons = @lessons.where('date > ?', Time.current)
+                               .order(date: :asc)
+                               .first(5)
+                               
+    @recent_payments = @lessons.order(date: :desc).first(5)
+  end
 end
