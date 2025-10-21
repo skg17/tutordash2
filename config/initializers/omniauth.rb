07a8945034ca27cfee4013f0903252e7846af729
Sahil Goonraz <sahil.goonraz@solirius.com>
2025-10-21 13:55:56 +0100

    Fix missing refresh token after re-linking Google account

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -9,6 +9,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
         Rails.application.credentials.dig(:google, :client_secret),
         {
             include_granted_scopes: true,
-            scope: 'email profile offline https://www.googleapis.com/auth/calendar',
+            scope: 'email, profile, https://www.googleapis.com/auth/calendar',
+            access_type: 'offline',
+            prompt: 'consent',
         }
 end
